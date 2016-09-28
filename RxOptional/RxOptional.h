eda5a202b3e97737e06e223b73144897ce008577
Leone Parise <leone.parise@gmail.com>
2016-09-28 20:14:18 -0300

    Update Carthage project

---
+++ RxOptional/RxOptional.h
@@ -0,0 +1,19 @@
+//
+//  RxOptional.h
+//  RxOptional
+//
+//  Created by Leone Parise Vieira da Silva on 28/09/16.
+//  Copyright © 2016 com.rxswiftcommunity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxOptional.
+FOUNDATION_EXPORT double RxOptionalVersionNumber;
+
+//! Project version string for RxOptional.
+FOUNDATION_EXPORT const unsigned char RxOptionalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxOptional/PublicHeader.h>
+
+
